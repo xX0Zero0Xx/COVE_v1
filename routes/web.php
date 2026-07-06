2d25eddd7699b7fded913ed2b51bfd4164f5e1b7
Lozada <lozadacesar1218@gmail.com>
2026-07-05 21:39:08 -0600

    Vista de inicio de sesion actualizada, mas o menos

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    register_shutdown_function(function () {
+        $error = error_get_last();
+        if ($error && $error['type'] === E_ERROR) {
+            // Handle fatal error
+            // You can log the error or display a custom error page
+            // For example, you can log the error message:
+            \Log::error('Fatal error: ' . $error['message']);
+        }
+    });
 });
